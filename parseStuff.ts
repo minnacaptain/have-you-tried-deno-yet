1f6c4d430d6d188b8271e4cbee1e8316c2c5f83e
Minna Niemi <35032643+minnacaptain@users.noreply.github.com>
2021-07-23 15:39:50 +0300

    Calculate average score

--- parseStuff.ts
+++ parseStuff.ts
@@ -1,8 +1,12 @@
-const getContent = () => Deno.readTextFile('./example_data.txt')
+const getContent = () => Deno.readTextFile("./example_data.txt");
 
-const calculateAverageScore = (getContent: () => Promise<string>) => getContent().then(result => {
-    const lines = result.split("\n").map(l => l.split(","))
-    console.log(lines)
-})
+const calculateAverageScore = (getContent: () => Promise<string>) =>
+    getContent().then((result) => {
+        const lines = result.split("\n")
+            .map((l) => l.split(","))
+            .filter((_, i) => i !== 0);
+        const scores = lines.map((l) => Number(l[3]));
+        return scores.reduce((a, c) => a + c) / scores.length;
+    });
 
-calculateAverageScore(getContent)
+console.log("Average score: " + await calculateAverageScore(getContent));
